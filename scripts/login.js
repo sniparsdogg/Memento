40c4addd57de21826b15f04b53872e3e5767b276
inesgmarcelino <inesgmarcelino@gmail.com>
2020-11-04 00:27:57 +0000

    update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -35,3 +35,52 @@ function principal() {
 /* ------------------------------------------------------------------------- */
 /*                                            REAÇÃO A EVENTOS DO UTILIZADOR */
 /* ------------------------------------------------------------------------- */
+
+/**
+ * Associa event handlers a elementos no documento HTML, em particular botões.
+ */
+function defineEventHandlersParaElementsHTML() {
+
+    document.getElementById(BOTAO_LOGIN).
+        addEventListener("click",fazerLogin);
+}
+
+/**
+ * Trata dos dados do Login, provinientes do formulário HTML
+ */
+function fazerLogin() {
+
+    let loginValid = formulario.reportValidity();
+    let username = null;
+    let senha = null;
+
+    if (loginValid) {
+
+        username = formulario.elements[USERNAME].value;
+        senha = formulario.elements[PASSWORD].value;
+
+        for (var x = 0; x <= localStorage.length; x++) {
+
+            if (x == localStorage.length) {
+                alert('Username ou Password incorreta!');
+                break;
+            }
+
+            let user = JSON.parse(localStorage.getItem("User" + String(x)));
+
+            if (user.username == username) {
+                if (user.senha == senha) {
+
+                    user.login = true;
+
+                    localStorage.setItem("User" + String(localStorage.length), JSON.stringify(user));
+                    alert("O seu Login foi executado com sucesso!");
+
+                    formulario.reset();
+                    window.location.href = "memento.html";
+                    break;
+                }
+            }
+        }
+    }
+}
